f3cce6512cf4ff8c97ca799092cfc659eef7fd5c
REQUIEMHR <110291579+REQUIEMHR@users.noreply.github.com>
2025-12-11 14:12:00 +0800

    基础部分简单完结

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d50921c5348762a5bc467595ae85d041106223")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65641da22a97da43ea9b32c6e1eeca398ab5c70a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
